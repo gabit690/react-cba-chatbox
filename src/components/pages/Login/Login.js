3db048bdc39c82c2103bb04a9214fc8050c568f8
gabit <gabit690@gmail.com>
2020-09-05 20:07:39 -0300

    Componente Title del Login realizado

---
+++ src/components/pages/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from './components/Title';
+
+const Login = () => {
+  return (
+    <div>
+      <Title text='Contenido Título' />
+      <p>Subtítulo</p>
+    </div>
+  );
+};
+
+export default Login;
