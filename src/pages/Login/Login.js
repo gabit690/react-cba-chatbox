483d58df45f278934f560ffad43534bfc2013506
gabit <gabit690@gmail.com>
2020-09-17 19:28:16 -0300

    Funcionalidad básica del Login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -10,54 +10,105 @@ const Login = () => {
   const [ user, setUser ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ passwordError, setPasswordError ] = useState(false);
+  const [ isLogin, setIsLogin ] = useState(false);
+  const [ hasError, setHasError ] = useState(false);
 
   function handleChange(name, value) {
     if(name === 'usuario') {
       setUser(value);
+      setHasError(false);
     } else {
       if (value.length < 6) {
         setPasswordError(true);
+        setHasError(false);
       } else {
-        setPassword(value);
         setPasswordError(false);
+        setPassword(value);
+        setHasError(false);
       }
     }
   }
 
+  function ifMatch(param) {
+    if (param.user.length > 0 && param.password.length > 0) {
+      if (param.user === 'Admin' && param.password === '123456') {
+        const { user, password } = param;
+        let ac = { user, password };
+        let account = JSON.stringify(ac);
+        localStorage.setItem('account', account);
+        setIsLogin(true);
+      } else {
+          setIsLogin(false);
+          setHasError(true);
+      }
+    } else {
+        setIsLogin(false);
+        setHasError(true);
+    }
+  }
+
   function handleSubmit() {
-    let account = { user, password};
+    let account = { user, password };
     if (account) {
-      console.log('Account: ', account)
+      ifMatch(account);
     }
   }
 
   return (
     <div className='loginContainer'>
-      <Title text='¡Bienvenid@!' />
-      <Label text='Usuario' styles='loginLabel' />
-      <Input
-       attribute= {{
-        id: 'usuario',
-        name: 'usuario',
-        type: 'text',
-        placeholder: 'Ingrese su usuario'
-       }}
-       handleChange={handleChange}
-      />
-      <Label text='Contraseña' styles='loginLabel' />
-      <Input
-       attribute= {{
-        id: 'contraseña',
-        name: 'contraseña',
-        type: 'password',
-        placeholder: 'Ingrese su contraseña'
-       }}
-       handleChange={handleChange}
-       param={passwordError}
-      />
-      <button className='loginButton' onClick={handleSubmit}>
-        Ingresar
-      </button>
+    { isLogin ? 
+        <div className='homeContainer'>
+          <h1>¡Hola, {user}</h1>
+          <Label text='Felicitaciones, estás logueado.' styles='homeLabel' />
+        </div>
+      :
+      <div className='loginContent'>
+        <Title text='¡Bienvenid@!' />
+        {
+          hasError &&
+          <Label 
+            text='Su contraseña o usuario son incorrectos, o no existen en nuestra plataforma.' 
+            styles='labelAlert' 
+          />
+        }
+        <Label 
+          text='Usuario'
+          styles='loginLabel' 
+        />
+        <Input
+        attribute= {{
+          id: 'usuario',
+          name: 'usuario',
+          type: 'text',
+          placeholder: 'Ingrese su usuario'
+        }}
+        handleChange={handleChange}
+        />
+        <Label
+        text='Contraseña'
+        styles='loginLabel'
+        />
+        <Input
+        attribute= {{
+          id: 'contraseña',
+          name: 'contraseña',
+          type: 'password',
+          placeholder: 'Ingrese su contraseña'
+        }}
+        handleChange={handleChange}
+        param={passwordError}
+        />
+        { passwordError && 
+          <Label 
+            text='Contraseña inválida o incompleta' styles='labelError'
+          />
+        }
+        <button className='loginButton' onClick={handleSubmit}>
+          Ingresar
+        </button>
+      </div>
+
+    }
     </div>
   );
 };
